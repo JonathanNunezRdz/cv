95975c448d8a99612f4adf6561968121ba025167
Jonathan Nunez <jonathannunezr1@gmail.com>
2025-11-05 22:36:36 -0600

    fixed base

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import { defineConfig } from 'vite'
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    "alias": {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+export default defineConfig(({ command, mode }) => {
+	return {
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
+		base: command === 'build' && mode === 'production' ? '/cv/' : '/',
+	};
+});
